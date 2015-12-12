c88abd23e22439ba6c6eb3d4f45e25f31ad96dc0
Elsk Hong <jacky8hyf@gmail.com>
2015-12-11 20:54:37 -0800

    trace to production

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(function(req, res, next) {
 // error handlers
 
 // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-if (app.get('env') === 'development') {
+if (true && app.get('env') === 'development') {
   app.use(function(err, req, res, next) { // jshint ignore:line
     var statusCode = err.status || 500;
     if(statusCode === 500) {
